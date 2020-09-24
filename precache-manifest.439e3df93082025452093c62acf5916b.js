8e2dde0cd64a92b9814da586d2db5954ebf1ec3a
bom <sebomujy@gmail.com>
2020-09-24 21:25:34 +0900

    Updates

---
+++ precache-manifest.439e3df93082025452093c62acf5916b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d19d9e3dd95bffabecf9d85c38ef739",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "221b91f921828dbe6b21",
+    "url": "/movie_app/static/css/main.771d600d.chunk.css"
+  },
+  {
+    "revision": "2cb00f0b337cb1b1485d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "221b91f921828dbe6b21",
+    "url": "/movie_app/static/js/main.a9b2d1ce.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
